f305aff202dca955ae58a373292a5c3e9eb31031
ishwars@valueaddsofttech.com <ishwars@valueaddsofttech.com>
2026-01-01 14:20:28 +0530

    file removed

--- tests/test_git.py
+++
@@ -1,5 +0,0 @@
-from pages.login_page import LoginPage
-from pages.compliance_page import CompliancePage
-from pages.citation_page import CitationPage
-from utils.config import USERNAME, PASSWORD
-import os
