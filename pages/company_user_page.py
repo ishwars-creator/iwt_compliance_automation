5666fce704ae5e6edb5c99e00a830731677caf31
ishwars@valueaddsofttech.com <ishwars@valueaddsofttech.com>
2026-03-23 17:38:05 +0530

    code updated 23-march-2026  at 05.38 PM

--- pages/company_user_page.py
+++ pages/company_user_page.py
@@ -13,73 +13,21 @@ class CompanyUserPage(BasePage):
         self.page.get_by_text(company_name).click()
         expect(self.page.get_by_role("heading", name=company_name)).to_be_visible()
         
-    def add_corp_admin(self,data):
+    #### Bleow is the one common function to add the all roles user  
+    
+    def add_user (self, first_name, last_name, role):
         self.page.locator(L.ADD_USER_BTN).click()
-        self.type(L.FIRSTNAME, data["corp_admin_firstname"])
-        self.type(L.LASTNAME, data["corp_admin_lastname"])
-        
-        # Generate a unique email using timestamp
-        corpadmin_email = f"{data['corp_admin_firstname']}_{data['corp_admin_lastname']}_{datetime.now().timestamp()}@yopmail.com"
-        self.type(L.EMAIL, corpadmin_email)
-        self.type(L.CONFIRM_EMAIL, corpadmin_email)
-        self.page.locator("#role").select_option(label="Corp Admin")  
-        self.page.locator(L.ADD_BTN).click()
-        self.page.locator(L.ADD_BTN).click()
-        self.page.wait_for_timeout(3000)
-        expect(self.page.get_by_text("User added successfully")).to_be_visible(timeout=15000)
-        
-    def add_corp_manager(self,data):
-        self.page.locator(L.ADD_USER_BTN).click()
-        self.type(L.FIRSTNAME, data["corp_manager_firstname"])
-        self.type(L.LASTNAME, data["corp_manager_lastname"])
-        corpmanager_email = f"{data['corp_manager_firstname']}_{data['corp_manager_lastname']}_{datetime.now().timestamp()}@yopmail.com"
-        self.type(L.EMAIL, corpmanager_email)
-        self.type(L.CONFIRM_EMAIL, corpmanager_email)
-        self.page.locator("#role").select_option(label="Corp Manager")
-        self.page.locator(L.ADD_BTN).click()
-        self.page.locator(L.ADD_BTN).click()
-        self.page.wait_for_timeout(3000)
-        expect(self.page.get_by_text("User added successfully")).to_be_visible(timeout=15000)
-        
-        
-    def add_mine_admin(self,data):
-        self.page.locator(L.ADD_USER_BTN).click()
-        self.type(L.FIRSTNAME, data["mine_admin_firstname"])
-        self.type(L.LASTNAME, data["mine_admin_lastname"])
-        mineadmin_email = f"{data['mine_admin_firstname']}_{data['mine_admin_lastname']}_{datetime.now().timestamp()}@yopmail.com"
-        self.type(L.EMAIL, mineadmin_email)
-        self.type(L.CONFIRM_EMAIL, mineadmin_email)
-        self.page.locator("#role").select_option(label="Mine Admin")
-        self.page.locator("select[name='mineId']").select_option(value="115") # value =115 ("Montevallo Quarry & Mill ")
-        self.page.locator(L.ADD_BTN).click()
-        self.page.locator(L.ADD_BTN).click()
-        self.page.wait_for_timeout(3000)
-        expect(self.page.get_by_text("User added successfully")).to_be_visible(timeout=15000)
-        
-    def add_mine_manager(self,data):
-        self.page.locator(L.ADD_USER_BTN).click()
-        self.type(L.FIRSTNAME, data["mine_manager_firstname"])
-        self.type(L.LASTNAME, data["mine_manager_lastname"])
-        minemanager_email = f"{data['mine_manager_firstname']}_{data['mine_manager_lastname']}_{datetime.now().timestamp()}@yopmail.com"
-        self.type(L.EMAIL, minemanager_email)
-        self.type(L.CONFIRM_EMAIL, minemanager_email)
-        self.page.locator("#role").select_option(label="Mine Manager")
-        self.page.locator("select[name='mineId']").select_option(value="115") # value =115 ("Montevallo Quarry & Mill ")
-        self.page.locator(L.ADD_BTN).click()
-        self.page.locator(L.ADD_BTN).click()
-        self.page.wait_for_timeout(3000)
-        expect(self.page.get_by_text("User added successfully")).to_be_visible(timeout=15000)
-        
-    def add_mine_user(self,data):
-        self.page.locator(L.ADD_USER_BTN).click()
-        self.type(L.FIRSTNAME, data["mine_user_firstname"])
-        self.type(L.LASTNAME, data["mine_user_lastname"])
-        mineuser_email = f"{data['mine_user_firstname']}_{data['mine_user_lastname']}_{datetime.now().timestamp()}@yopmail.com"
+        self.type(L.FIRSTNAME, first_name)
+        self.type(L.LASTNAME, last_name)
+        mineuser_email = f"{first_name}_{last_name}_{datetime.now().timestamp()}@yopmail.com"
         self.type(L.EMAIL, mineuser_email)
         self.type(L.CONFIRM_EMAIL, mineuser_email)
-        self.page.locator("#role").select_option(label="Mine User")
-        self.page.locator("select[name='mineId']").select_option(value="115") # value =115 ("Montevallo Quarry & Mill ")
+        self.page.locator("#role").select_option(label=role)
+        if role in ["Mine Admin","Mine User","Mine Manager"]:
+            self.page.locator("select[name='mineId']").select_option(value="115") # value =115 ("Montevallo Quarry & Mill ")
+        
         self.page.locator(L.ADD_BTN).click()
         self.page.locator(L.ADD_BTN).click()
         self.page.wait_for_timeout(3000)
-        expect(self.page.get_by_text("User added successfully")).to_be_visible(timeout=15000)           
+        expect(self.page.get_by_text("User added successfully")).to_be_visible(timeout=15000)
+            
--- tests/test_company_users.py
+++ tests/test_company_users.py
@@ -10,9 +10,8 @@ def test_add_corp_admin(page):
     cp = CompanyUserPage(page)
     cp.navigate_to_settings_menu()
     cp.click_on_company_name("Lhoist Group")
-    cp.add_corp_admin(COMPANY_USERS)
-    
-    
+    cp.add_user(COMPANY_USERS["corp_admin_firstname"],COMPANY_USERS["corp_admin_lastname"],"Corp Admin")
+
 def test_add_corp_manager(page):
     login = LoginPage(page)
     login.open()
@@ -20,7 +19,8 @@ def test_add_corp_manager(page):
     cp = CompanyUserPage(page)
     cp.navigate_to_settings_menu()
     cp.click_on_company_name("Lhoist Group")
-    cp.add_corp_manager(COMPANY_USERS)    
+    cp.add_user(COMPANY_USERS["corp_manager_firstname"],COMPANY_USERS["corp_manager_lastname"],"Corp Manager")
+    
     
 def test_add_mine_admin(page):
     login = LoginPage(page)
@@ -29,7 +29,7 @@ def test_add_mine_admin(page):
     cp = CompanyUserPage(page)
     cp.navigate_to_settings_menu()
     cp.click_on_company_name("Lhoist Group")
-    cp.add_mine_admin(COMPANY_USERS)
+    cp.add_user(COMPANY_USERS["mine_admin_firstname"],COMPANY_USERS["mine_admin_lastname"],"Mine Admin")
 
 def test_add_mine_manager(page):
     login = LoginPage(page)
@@ -38,7 +38,7 @@ def test_add_mine_manager(page):
     cp = CompanyUserPage(page)
     cp.navigate_to_settings_menu()
     cp.click_on_company_name("Lhoist Group")
-    cp.add_mine_manager(COMPANY_USERS)
+    cp.add_user(COMPANY_USERS["mine_manager_firstname"],COMPANY_USERS["mine_manager_lastname"],"Mine Manager")
     
 def test_add_mine_user(page):
     login = LoginPage(page)
@@ -47,4 +47,22 @@ def test_add_mine_user(page):
     cp = CompanyUserPage(page)
     cp.navigate_to_settings_menu()
     cp.click_on_company_name("Lhoist Group")
-    cp.add_mine_user(COMPANY_USERS) 
+    cp.add_user(COMPANY_USERS["mine_user_firstname"],COMPANY_USERS["mine_user_lastname"],"Mine User")        
+
+
+########################
+"""Beow is the block of clode whic adds the all users in one flow 
+"""
+        
+def test_add_compnay_user_flow(page):
+    login = LoginPage(page)
+    login.open()
+    login.login(USERNAME, PASSWORD)
+    cp = CompanyUserPage(page)
+    cp.navigate_to_settings_menu()
+    cp.click_on_company_name("Lhoist Group")
+    cp.add_user(COMPANY_USERS["corp_admin_firstname"],COMPANY_USERS["corp_admin_lastname"],"Corp Admin")
+    cp.add_user(COMPANY_USERS["corp_manager_firstname"],COMPANY_USERS["corp_manager_lastname"],"Corp Manager")
+    cp.add_user(COMPANY_USERS["mine_admin_firstname"],COMPANY_USERS["mine_admin_lastname"],"Mine Admin")
+    cp.add_user(COMPANY_USERS["mine_manager_firstname"],COMPANY_USERS["mine_manager_lastname"],"Mine Manager")
+    cp.add_user(COMPANY_USERS["mine_user_firstname"],COMPANY_USERS["mine_user_lastname"],"Mine User")
